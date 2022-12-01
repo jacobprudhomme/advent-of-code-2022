e4c7c6885b179286556e2949531c551700daf5f6
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2022-12-01 21:20:30 +0100

    Solved part 2

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -23,7 +23,27 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val maxesSoFar = intArrayOf(0, 0, 0)
+        var currentSum = 0
+        for (line in input) {
+            if (line.isEmpty()) {
+                val (idx, smallestMaxSoFar) = maxesSoFar.withIndex().minBy { (_, maxVal) -> maxVal }
+                if (currentSum > smallestMaxSoFar) {
+                    maxesSoFar[idx] = currentSum
+                }
+
+                currentSum = 0
+            } else {
+                currentSum += line.toInt()
+            }
+        }
+
+        val (idx, smallestMaxSoFar) = maxesSoFar.withIndex().minBy { (_, maxVal) -> maxVal }
+        if (currentSum > smallestMaxSoFar) {
+            maxesSoFar[idx] = currentSum
+        }
+
+        return maxesSoFar.sum()
     }
 
     val input = readInput("input")
