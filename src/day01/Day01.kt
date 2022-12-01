e8bc902a9d2c7086521a25cf0c63306b7ca14cfc
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2022-12-01 21:20:00 +0100

    Solved part 1

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -5,7 +5,21 @@ fun main() {
         .readLines()
 
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxSoFar = 0
+        var currentSum = 0
+        for (line in input) {
+            if (line.isEmpty()) {
+                if (currentSum > maxSoFar) {
+                    maxSoFar = currentSum
+                }
+
+                currentSum = 0
+            } else {
+                currentSum += line.toInt()
+            }
+        }
+
+        return if (currentSum > maxSoFar) currentSum else maxSoFar
     }
 
     fun part2(input: List<String>): Int {
