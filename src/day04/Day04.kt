f3efdba512aaa46cab741696a0f7bb656cc8bc8e
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2022-12-04 18:18:20 +0100

    Solved part 2

--- src/day04/Day04.kt
+++ src/day04/Day04.kt
@@ -4,6 +4,16 @@ fun main() {
     fun readInput(name: String) = File("src/day04", name)
         .readLines()
 
+    fun processInput(line: String): Pair<Pair<Int, Int>, Pair<Int, Int>> {
+        val (range1, range2, _) = line.split(",").map { range ->
+            range.split("-").map { it.toInt() }.let { (start, end, _) ->
+                Pair(start, end)
+            }
+        }
+
+        return Pair(range1, range2)
+    }
+
     fun sign(n: Int): Int =
         if (n > 0) {
             1
@@ -22,14 +32,13 @@ fun main() {
         return (sign(startDiff) >= 0) and (sign(endDiff) <= 0)
     }
 
+    fun Pair<Int, Int>.overlaps(otherRange: Pair<Int, Int>): Boolean =
+        (this.first <= otherRange.second) and (otherRange.first <= this.second)
+
     fun part1(input: List<String>): Int {
         var count = 0
         for (line in input) {
-            val (range1, range2, _) = line.split(",").map { range ->
-                range.split("-").map { it.toInt() }.let { (start, end, _) ->
-                    Pair(start, end)
-                }
-            }
+            val (range1, range2) = processInput(line)
 
             if (range1.contains(range2) or range2.contains(range1)) {
                 ++count
@@ -40,7 +49,16 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var count = 0
+        for (line in input) {
+            val (range1, range2) = processInput(line)
+
+            if (range1.overlaps(range2)) {
+                ++count
+            }
+        }
+
+        return count
     }
 
     val input = readInput("input")
