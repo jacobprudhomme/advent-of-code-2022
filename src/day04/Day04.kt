e15792243d262f1c1ef91e38d0c16e9103f2571e
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2022-12-06 11:48:18 +0100

    Cleaned up day 04

--- src/day04/Day04.kt
+++ src/day04/Day04.kt
@@ -15,15 +15,9 @@ fun main() {
     }
 
     fun sign(n: Int): Int =
-        if (n > 0) {
-            1
-        } else {
-            if (n < 0) {
-                -1
-            } else {
-                0
-            }
-        }
+        if      (n > 0) { 1 }
+        else if (n < 0) { -1 }
+        else            { 0 }
 
     fun Pair<Int, Int>.contains(otherRange: Pair<Int, Int>): Boolean {
         val startDiff = this.first - otherRange.first
