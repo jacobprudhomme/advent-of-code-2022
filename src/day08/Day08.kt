1e274834d8e080266a2c930f63268d3e77357e36
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2022-12-09 17:54:25 +0100

    Solved part 2

--- src/day08/Day08.kt
+++ src/day08/Day08.kt
@@ -49,6 +49,25 @@ fun main() {
                 or (smallerBelow(mtx, tree, pos) == height - pos.first - 1))
     }
 
+    fun calculateVisibilityScore(mtx: Matrix<Int>, tree: Int, pos: Pair<Int, Int>): Int {
+        val (row, col) = pos
+        val (height, width) = mtx.dimensions()
+
+        var stl = smallerToLeft(mtx, tree, pos)
+        if (stl < col) { ++stl }
+
+        var str = smallerToRight(mtx, tree, pos)
+        if (str < width - col - 1) { ++str }
+
+        var sa = smallerAbove(mtx, tree, pos)
+        if (sa < row) { ++sa }
+
+        var sb = smallerBelow(mtx, tree, pos)
+        if (sb < height - row - 1) { ++sb }
+
+        return (stl * str * sa * sb)
+    }
+
     fun part1(input: List<String>): Int {
         val grid = Matrix(input.map { row -> row.map(Char::digitToInt) })
 
@@ -65,7 +84,22 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val grid = Matrix(input.map { row -> row.map(Char::digitToInt) })
+
+        val treesSeen = mutableSetOf<Pair<Int, Int>>()
+        var maxScoreSeenSoFar = 0
+        grid.forEachIndexed { rowIdx, colIdx, tree ->
+            if (Pair(rowIdx, colIdx) !in treesSeen) {
+                val visibilityScore = calculateVisibilityScore(grid, tree, Pair(rowIdx, colIdx))
+                if (visibilityScore > maxScoreSeenSoFar) {
+                    maxScoreSeenSoFar = visibilityScore
+                }
+
+                treesSeen.add(Pair(rowIdx, colIdx))
+            }
+        }
+
+        return maxScoreSeenSoFar
     }
 
     val input = readInput("input")
